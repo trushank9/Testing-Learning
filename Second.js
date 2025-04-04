b39f0a6ba5536601af7b078e54d2f8c6016f3e74
trushank9 <trushank91@gmail.com>
2025-04-04 16:55:12 +0530

    Basic condition

---
+++ Second.js
@@ -0,0 +1,15 @@
+//condition 
+let numberguest =14;
+let pizzsize;
+
+if (numberguest<=3){
+    pizzsize="Small";
+}
+else if (numberguest<=5){
+pizzsize="Medium";
+}
+else{
+pizzsize="Large";
+}
+
+console.log("print the size of piza", pizzsize)
